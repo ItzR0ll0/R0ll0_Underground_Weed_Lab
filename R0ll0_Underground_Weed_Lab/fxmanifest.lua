1503eb89786a8a1d25119f4123521b2f919d3cd9
ItzR0ll0 <dude30804@gmail.com>
2026-07-11 14:25:00 +0200

    Add files via upload

---
+++ R0ll0_Underground_Weed_Lab/fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+version '1.0'
+author 'R0ll0'
+description 'Underground Weed Lab MLO'
+
+files {
+    'r0ll0_weed_lab_manifest.ymf',
+
+    'stream/[ybn]/*.ybn',
+    'stream/[ydr]/*.ydr',
+    'stream/[ymap]/*.ymap',
+    'stream/[ytyp]/*.ytyp',
+
+    'stream/[vanilla]/*.ybn',
+    'stream/[vanilla]/*.ydr',
+    'stream/[vanilla]/*.ymap'
+}
+
+data_file 'DLC_ITYP_REQUEST' 'r0ll0_weed_lab_manifest.ymf'
